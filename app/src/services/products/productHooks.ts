4348423692328e773d493b645d15332f45e5c6f5
Aruk Pupatee <aruk.pupatee@gmail.com>
2022-04-18 12:22:22 +0700

    [Refactor]: Product interface to model

--- app/src/services/products/productHooks.ts
+++ app/src/services/products/productHooks.ts
@@ -1,27 +1,21 @@
 import { range } from 'ramda';
 import { useEffect, useState } from 'react';
-
-interface Product {
-  id: string;
-  name: string;
-  imageUrl: string;
-  price: number;
-  quantity: number;
-  isAvailable: boolean;
-}
+import { Product } from './models/Product';
 
 export const useListProducts = (): { products: Product[] } => {
   const [products, setProducts] = useState<Product[]>([]);
 
   useEffect(() => {
-    const products = range(1, 14).map((n) => ({
-      id: `${n}`,
-      name: `Product${n}`,
-      imageUrl: 'https://place-hold.it/500x500',
-      price: n * 10,
-      quantity: n === 5 ? 0 : 10,
-      isAvailable: n === 5 ? false : true
-    }));
+    const products = range(1, 14)
+      .map((n) => ({
+        id: `${n}`,
+        name: `Product${n}`,
+        imageUrl: 'https://place-hold.it/500x500',
+        price: n * 10,
+        quantity: n === 5 ? 0 : 10,
+        isAvailable: n === 5 ? false : true
+      }))
+      .map((data) => new Product(data));
 
     setProducts(products);
   }, []);
